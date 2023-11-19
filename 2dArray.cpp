4db00e1e753de50bc1c227b339c4fbfe40e2ac47
gurleen-kaurr <gurleen.kaurrrrrr@gmail.com>
2023-11-20 02:44:01 +0530

    Create 2D Array

---
+++ 2dArray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int arr[3][3];
+
+    cout<<"Enter the elements in the array: ";
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+     cout<<"Printing the elements: "<<endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
