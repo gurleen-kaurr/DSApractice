33cf105aab0cfa2b1575e129a6b52177db4722c4
gurleen-kaurr <gurleen.kaurrrrrr@gmail.com>
2024-10-13 21:46:42 +0530

    update

---
+++ LinkedList/doublyLinkedList.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    int data ;
+    node* prev ;
+    node* next ;
+    node(int data){
+        this -> data = data ;
+        this -> prev = NULL ;
+        this -> next = NULL ;
+    }
+}
+
+void travereList(node* head){
+
+}
+
+int main(){
+    
+}
